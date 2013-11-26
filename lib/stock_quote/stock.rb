1d6e56d67a59e81cd06d8b01bfe0a300f0596ad5
Ty Rauber <tyrauber@mac.com>
2013-11-25 16:11:39 -0800

    master :: Extend String Class with to_date method. Pass all strings through it looking for date matches and format every date type field to "YYYY-MM-DD".

--- lib/stock_quote/stock.rb
+++ lib/stock_quote/stock.rb
@@ -28,7 +28,7 @@ module StockQuote
       else
         @response_code = 200
         data.map do |k,v|
-          instance_variable_set("@#{k.underscore}", (v.nil? ? nil : v.to_fs))
+          instance_variable_set("@#{k.underscore}", (v.nil? ? nil : v.to_date.to_fs))
         end
       end
     end
--- lib/stock_quote/utility.rb
+++ lib/stock_quote/utility.rb
@@ -1,3 +1,4 @@
+require 'date'
 class String
 
   def underscore
@@ -14,6 +15,16 @@ class String
 
   def to_p
     self.split(',').to_p
+  end
+
+  def to_date
+    if self.match(/\d{4}-\d{2}-\d{2}/)
+      Date.strptime(self, "%Y-%m-%d").to_s
+    elsif self.match(/\d{2}\/\d{2}\/\d{4}/)
+      Date.strptime(self, "%m/%d/%Y").to_s
+    else
+      self
+    end
   end
 end
 class Array
